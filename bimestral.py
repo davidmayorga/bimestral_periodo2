f8a086687319b6e7c292c11fc1b09549d649d16d
davidmayorga <davidmayorga1@colegioguanenta.edu.co>
2022-06-16 08:34:42 -0500

    Bimestral 2 periodo

---
+++ bimestral.py
@@ -0,0 +1,46 @@
+
+
+
+from ast import While
+
+
+print("----------------------------------")
+print("-------- Cambio de cheques -------")
+print("----------------------------------")
+
+#Entrada
+
+Valor_del_cheque = int (input ('Ingresa el valor del cheque: '))
+total_monedas_de_100=0
+total_billetes_de_10000=0
+total_billetes_de_2000=0
+
+#Processing
+while Valor_del_cheque !=0:
+    monedas_de_100=Valor_del_cheque
+    billetes_de_10000=(monedas_de_100-monedas_de_100%10000)//10000
+    monedas_de_100=monedas_de_100%10000
+    billetes_de_2000=(monedas_de_100-monedas_de_100%2000)//2000
+    monedas_de_100=monedas_de_100%2000
+    monedas_de_100=(monedas_de_100-monedas_de_100%100)//100
+    monedas_de_100=monedas_de_100%100   
+    print ('Valor de billetes de 10000: ' + repr (billetes_de_10000))
+    print ('Valor de billetes de 2000: ' + repr (billetes_de_2000))
+    print ('Valor de monedas de 100: ' + repr (monedas_de_100))
+    total_monedas_de_100=total_monedas_de_100 + monedas_de_100
+    total_billetes_de_10000=total_billetes_de_10000 + billetes_de_10000
+    total_billetes_de_2000=total_billetes_de_2000 + billetes_de_2000
+    Valor_del_cheque = int (input ('Ingresa el valor del cheque: '))
+
+
+else:
+    print("Fin de ingreso de datos")
+
+#output
+
+print("El total de billetes de  diez mil en el dia fue de " + str(total_billetes_de_10000))
+print("El total de billetes de  dos mil en el dia fue de " + str(total_billetes_de_2000))
+print("El total de monedas de cien en el dia fue de " + str(total_monedas_de_100))
+
+
+
